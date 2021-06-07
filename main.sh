00f68ae4649fd93852a645e342969ab1dbfcf68e
Miguel <>
2021-06-07 12:45:19 +0000

    verificador de senha

--- main.sh
+++ main.sh
@@ -1,24 +1,35 @@
 #!usr/bin/env bash
 
-meu_nome="Meu nome é Miguel"
-data="e a data de hoje é: `date +%F`"
+#meu_nome="Meu nome é Miguel"
+#data="e a data de hoje é: `date +%F`"
 
 #echo -e "$meu_nome \n$data"
-echo "Qual é o seu nome?  "
+#echo "Qual é o seu nome?  "
 
-read nome
+#read nome
 
-echo -e "Tem certeza que seu nome é: $nome \n(y/n)"
+#echo -e "Tem certeza que seu nome é: $nome \n(y/n)"
 
-read choice
+#read choice
 
-echo "Qual é sua senha?"
+#echo "Qual é sua senha?"
 
+#read senha
+
+#echo "Confirme a senha: "
+
+#read csenha
+
+#test senha = csenha
+#echo $?
+
+echo "Digite sua senha:  "
 read senha
+echo "Confirme sua senha:  "
+read verificarsenha
 
-echo "Confirme a senha: "
-
-read csenha
-
-test senha = csenha
-echo $?
+if [[ "$senha" == "$verificarsenha" ]]; then
+  echo "A senha é igual, pode prosseguir"
+else 
+  echo "A senha não bate o senhor não pode prosseguir"
+fi
